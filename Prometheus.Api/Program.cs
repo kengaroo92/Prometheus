63b088aae0e34e6b9d67fe02f1739625566a6939
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-17 21:12:35 -0600

    Added code to allow CORS configuration.

--- Prometheus.Api/Program.cs
+++ Prometheus.Api/Program.cs
@@ -4,12 +4,14 @@ using Prometheus.Api.Data;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// Add services to the container.
 // We're using the GetConfiguration method (defined below) to read the configuration
 // during both build time and runtime. This is necessary to ensure that EF Core tools
 // and the runtime pipeline both read the configuration correctly.
 string connectionString = Environment.GetEnvironmentVariable("DefaultConnection") ?? builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddCors(); // Add CORS support to the API
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,6 +29,16 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(policy =>
+{
+    policy
+    .WithOrigins("*") // Eventually replace with the allowed origins. For now it allows any origin to connect.
+    .AllowAnyMethod() // Allow any method. Can specify which methods to allow. Ex. ("GET", "POST").
+    .AllowAnyHeader() // Allow any header. Can specify which headers to allow. Ex. ("Content-Type", "Authorization").
+    .WithExposedHeaders("Content-Disposition"); // Custom headers you want to expose in the response.
+});
+
+// Make sure to keep this after the UseCors() method. It's essential for the middleware pipeline to work correctly.
 app.UseAuthorization();
 
 app.MapControllers();
