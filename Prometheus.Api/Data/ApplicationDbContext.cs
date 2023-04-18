e977541a756a43c4dca9fe422fe94b17d1c5d0a4
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-17 19:26:18 -0600

    Added DbContext and ConnectionString for the PostgreSQL database.

---
+++ Prometheus.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Prometheus.Api.Models;
+
+namespace Prometheus.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Proposal> Proposals { get; set; }
+        public DbSet<Revenue> Revenues { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
