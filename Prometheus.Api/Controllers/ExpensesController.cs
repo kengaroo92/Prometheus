5c66ae16ae59430ae1afb3263f912a8f32857b36
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-17 20:56:31 -0600

    Added CRUD operations for ExpensesControoler and RevenueController.

---
+++ Prometheus.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prometheus.Api.Data;
+using Prometheus.Api.Models;
+
+namespace Prometheus.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpensesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Expenses
+        // This method retrieves a list of all expenses.
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Expense>>> GetExpenses()
+        {
+            return await _context.Expenses
+                .Include(e => e.Project)
+                .ToListAsync();
+        }
+
+        // GET: api/Expenses/5
+        // This method retrieves a specific expense by ID.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Expense>> GetExpense(int id)
+        {
+            var expense = await _context.Expenses
+                .Include(e => e.Project)
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return expense;
+        }
+
+        // PUT: api/Expenses/5
+        // This method updates an existing expense by ID.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateExpense(int id, Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(expense).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Expenses
+        // This method creates a new expense.
+        [HttpPost]
+        public async Task<ActionResult<Expense>> CreateExpense(Expense expense)
+        {
+            _context.Expenses.Add(expense);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetExpense), new { id = expense.Id }, expense);
+        }
+
+        // DELETE: api/Expenses/5
+        // This method deletes a specific expense by ID.
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteExpense(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Helper method to check if an expense with the given ID exists.
+        private bool ExpenseExists(int id)
+        {
+            return _context.Expenses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Prometheus.Api/Controllers/RevenueController.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prometheus.Api.Data;
+using Prometheus.Api.Models;
+
+namespace Prometheus.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RevenuesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RevenuesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Revenues
+        // This method retrieves a list of all revenues.
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Revenue>>> GetRevenues()
+        {
+            return await _context.Revenues
+                .Include(r => r.Project)
+                .Include(r => r.Invoice)
+                .ToListAsync();
+        }
+
+        // GET: api/Revenues/5
+        // This method retrieves a specific revenue by ID.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Revenue>> GetRevenue(int id)
+        {
+            var revenue = await _context.Revenues
+                .Include(r => r.Project)
+                .Include(r => r.Invoice)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (revenue == null)
+            {
+                return NotFound();
+            }
+
+            return revenue;
+        }
+
+        // PUT: api/Revenues/5
+        // This method updates an existing revenue by ID.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateRevenue(int id, Revenue revenue)
+        {
+            if (id != revenue.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(revenue).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RevenueExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Revenues
+        // This method creates a new revenue.
+        [HttpPost]
+        public async Task<ActionResult<Revenue>> CreateRevenue(Revenue revenue)
+        {
+            _context.Revenues.Add(revenue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetRevenue), new { id = revenue.Id }, revenue);
+        }
+
+        // DELETE: api/Revenues/5
+        // This method deletes a specific revenue by ID.
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRevenue(int id)
+        {
+            var revenue = await _context.Revenues.FindAsync(id);
+            if (revenue == null)
+            {
+                return NotFound();
+            }
+
+            _context.Revenues.Remove(revenue);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Helper method to check if a revenue with the given ID exists.
+        private bool RevenueExists(int id)
+        {
+            return _context.Revenues.Any(r => r.Id == id);
+        }
+    }
+}
