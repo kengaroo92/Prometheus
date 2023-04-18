c176f313acfbb91896cd68ec7a841df0e6e19e9f
Kenan Bjelosevic <kenanbjelosevic92@gmail.com>
2023-04-17 20:36:33 -0600

    Added CRUD operations for ProjectsController.

---
+++ Prometheus.Api/Controllers/ProjectsController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Prometheus.Api.Data;
+using Prometheus.Api.Models;
+
+namespace Prometheus.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProjectsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Projects
+        // This method retrieves a list of all projects.
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return await _context.Projects.Include(p => p.Customer).ToListAsync();
+        }
+
+        // GET: api/Projects/5
+        // This method retrieves a specific project by ID.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Project>> GetProject(int id)
+        {
+            var project = await _context.Projects.Include(p => p.Customer).FirstOrDefaultAsync(p => p.Id == id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return project;
+        }
+
+        // PUT: api/Projects/5
+        // This method updates an existing project by ID.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProject(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProjectExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Projects
+        // This method creates a new project.
+        [HttpPost]
+        public async Task<ActionResult<Project>> CreateProject(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProject), new { id = project.Id }, project);
+        }
+
+        // DELETE: api/Projects/5
+        // This method deletes a specific project by ID.
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Helper method to check if a project with the given ID exists.
+        private bool ProjectExists(int id)
+        {
+            return _context.Projects.Any(e => e.Id == id);
+        }
+    }
+}
